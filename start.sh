2ee59f2f790c98857211491f6e6d8b07eea157f9
Thinkamey <162806639+Thinkamey@users.noreply.github.com>
2025-02-24 22:05:32 +0530

    updated

--- start.sh
+++ start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash 
-# Install FFmpeg
-apt-get update && apt-get install -y ffmpeg
-
-# Set execution permission
-chmod +x start.sh  
-
-# Start the Flask app using Gunicorn
-gunicorn -w 1 -b 0.0.0.0:5000 app:app --timeout 120
+gunicorn --preload -w 1 -b 0.0.0.0:5000 app:app --timeout 120
